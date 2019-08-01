8479319d0e2f00b3abaddbfbe85879deae76175b
闫冬 <yandong@smartisan.com>
2019-08-01 23:55:21 +0800

    php code

---
+++ Application/Home/Conf/config.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	 'DB_TYPE'  => 'mysql',// 数据库类型 
+	 'DB_HOST'  => 'localhost',// 数据库服务器地址
+	 'DB_NAME'  => 'yql',// 数据库名称
+	 'DB_USER'  => 'root',// 数据库用户名
+	 'DB_PWD'  => '',// 数据库密码
+	 'DB_PREFIX'  => 'yql_',// 数据表前缀
+	 'DB_CHARSET' => 'utf8',// 网站编码
+	 'DB_PORT'  => '3306',// 数据库端口
+	 'APP_DEBUG' =>  TRUE,// 开启调试模式
+	 'URL_MODEL' => 1, // 如果你的环境不支持PATHINFO 请设置为3
+	 'SHOW_PAGE_TRACE' =>TRUE, //开启页面Trace
+	 'DEFAULT_MODULE'     => 'Home',
+);
